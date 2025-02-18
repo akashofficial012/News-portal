c427e41d490214e4527a368f9bca258f627fc2f7
akashofficial012 <akashmondal.code@gmail.com>
2025-02-18 12:39:31 +0600

    create a login api

---
+++ controller/authController.js
@@ -0,0 +1,39 @@
+const authModel = require("../model/authModel");
+const bcrypt = require("bcrypt"); 
+const jwt = require("jsonwebtoken");
+
+class AuthController {
+  login = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({ message: "Email and password are required" });
+    }
+    try {
+      const user = await authModel.findOne({ email }).select("+password");
+
+      if (!user) {
+        return res.json({ message: "User not found" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return res.json({ message: "Invalid password" });
+      }
+      const payload = {
+        id: user.id,
+        name: user.name,
+        category: user.category,
+        role: user.role,
+      };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRATION || "1h",
+      });
+
+      return res.json({ message: "Login successful", token });
+    } catch (error) {
+      console.error("Login Error:", error);
+      return res.json({ message: "Internal Server Error" });
+    }
+  };
+}
+
+module.exports = new AuthController();
--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./db/connectDb");
+const router = require("./routes/authRoute");
 
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+connectDB();
+
+app.use("/api", router);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
