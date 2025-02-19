49050f459b901ae7995f83ed093a895907d64ca5
akashofficial012 <akashmondal.code@gmail.com>
2025-02-19 17:24:26 +0600

    useContext api with react

--- client/app/(admin)/dashboard/page.js
+++ client/app/(admin)/dashboard/page.js
@@ -1,7 +1,17 @@
-import React from 'react'
+"use client";
+
+import React, { useContext } from 'react'
+import storeContext from '@/context/storeContext';
 
 export default function page() {
+  const { store } = useContext(storeContext)
+    console.log(store.userInfo, 'store.userInfo');
+  //  const token = localStorage.getItem('newsToken')
+  //  console.log(token);
+   
+    
   return (
     <div>page</div>
   )
 }
+
--- client/app/layout.js
+++ client/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import StorePovider from "@/context/StorePovider";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +21,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+         <StorePovider>
+         <Toaster
+  position="top-center"
+  reverseOrder={false}
+/>
+          {children}
+        </StorePovider>
       </body>
     </html>
   );
